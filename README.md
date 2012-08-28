4cb1fefb617ce81b274974d7859f9c1ab59d1750
Matthias Huttar <mhuttar@ebay.com>
2012-08-28 22:17:23 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Travellor
 =========
 
-Very simple Javascript/PHP Application that allows to manage travel plans. 
+Very simple Javascript/PHP Application that allows to manage travel plans.
+
+Feel free to reuse the code as you like. There's not much there yet.  
---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="json2.js"></script>
+    <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true"></script>
+	<style type="text/css">
+	body, html, .maxheight {height: 100%; }
+	#maps-area{height: 100%; }
+	.new-item input { width: 90%; }
+	.new-item select {width: 100%; }
+	</style>
+  </head>
+  <body>
+    
+	<div class=" container-fluid maxheight"  >
+		<div class="row-fluid maxheight" >
+	    <div class="span9" id="maps-area">
+	    	asasdf
+	    </div>
+		<div class="span3" id="markers-area">
+
+		   <div class="navbar navbar-inverse">
+      			<div class="navbar-inner">
+					<div class="container">
+						<a class="brand" href="#">Travellor</a>
+						<div class="nav-collapse collapse">
+							<ul class="nav">
+								<li class="active"><a href="#" id="action-save">save</a></li>
+							</ul>
+      					</div><!--/.nav-collapse -->
+    				</div>
+				</div>
+			</div>
+
+
+
+			<form class="new-item">
+				
+				<input type="text" id="new-item-title" placeholder="Title" /><br/>
+				<input type="text" id="new-item-address" placeholder="Address" /><br/>
+				<select id="new-item-color">
+					<option>Blue</option>
+					<option>Green</option>
+					<option>Red</option>
+					<option>Yellow</option>
+				</select>
+				<p>
+					<button type="button" id="new-item-preview" class="btn btn-primary">Search</button>
+				</p>
+				
+				<p id="new-item-confirm-alert" class="alert fade in">
+					Is this the location you are looking for? <br/>
+					<button type="button" id="preview-confirm">Yes</button>
+					<button type="button" id="preview-cancel">No</button>
+				</p>
+			</form>
+
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Ico</th>
+						<th>Address</th>
+					</tr>
+				</thead>
+				<tbody id="data-table-body">
+					
+				</tbody>
+			</table>
+    	</div>
+    	</div>
+	</div>
+	    <script type="text/javascript">
+    	var cfg = {
+    		startLocation: "Hongkong",
+    		country: "HK"
+    	}; 
+    	
+    	var items = [];
+    	
+		$(document).ready(function(){
+			initMaps(); 
+			initUi(); 
+
+
+
+		}); 
+		
+		function initUi() {
+			$("#new-item-preview").click(function(){
+				var loc = $("#new-item-address").val();
+				var title = $("#new-item-title").val();
+				var color = $("#new-item-color").val();
+				
+				resolve(loc, function(data){
+					centerOn(data);
+					var previewMarker = marker.blank(data, color); 
+					$("#preview-confirm, #preview-cancel").unbind();
+					$("#new-item-confirm-alert").show();
+					$("#preview-confirm").click(function(){
+
+						var item = new MapItem(title, loc, data.lat, data.lng); 
+						item.setMarker(marker.nextOf(data, color)); 
+						
+						items.push(item);
+						addToTable(item);
+						previewMarker.setMap(null);						
+						$("#new-item-confirm-alert").hide(); 
+					});
+					$("#preview-cancel").click(function(){
+						previewMarker.setMap(null);
+						$("#new-item-confirm-alert").hide(); 
+					});
+				}); 
+			}); 
+			$("#new-item-confirm-alert").hide(); 
+			$("#action-save").click(function(e){ 
+				e.preventDefault(); 
+				var d = {"content": JSON.stringify(items) }; 
+				$.ajax("storeprefs.php",{data: d, dataType: "json", type:"POST"})
+					.done(function(){alert("Saved"); })
+					.fail(function(){alert("error"); }); 
+			});
+		}
+		
+		function addToTable(mapItem) {
+			var img = mapItem.marker.icon.url; 
+			var imgSrc = "<img src='"+img+"' />"; 
+			var html = "<tr><td>"+imgSrc+"</td><td><strong>"+mapItem.title+"</strong><br/>"+mapItem.address+"</td></tr>"; 
+			var item  = $(html);
+			console.log(item);
+			item.mouseover(function(){mapItem.marker.setAnimation(google.maps.Animation.BOUNCE);}); 
+			item.mouseout(function(){mapItem.marker.setAnimation(null);}); 
+			$("#data-table-body").append(item); 
+			console.log(mapItem);
+		}
+		
+		function MapItem(title, address, lat, lng) { 
+			this.title = title; 
+			this.address = address; 
+			this.lat = lat; 
+			this.lng = lng; 
+			this.setMarker = function(marker) {this.marker = marker; }
+		}
+		
+		function initMaps() {
+
+			var initialOpts = {
+				zoom: 11,
+
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			cfg.maps = new google.maps.Map(document.getElementById("maps-area"), initialOpts);
+ 			cfg.geocoder = new google.maps.Geocoder(); 
+			
+ 			resolve(cfg.startLocation, centerOn); 
+  
+		}
+		
+		function addMarker(data) {
+			var ll = new google.maps.LatLng(data.lat, data.lng);
+			var marker = new google.maps.Marker({position: ll, map: cfg.maps, title: data.search});
+			return marker; 
+		}
+		
+		function centerOn(data) {
+			var loc  = new google.maps.LatLng(data.lat, data.lng);
+			cfg.maps.setCenter(loc);
+		}
+		
+		function resolve(address, callback) {
+			cfg.geocoder.geocode( { 'address': address+", "+cfg.country}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					var res = results[0].geometry.location; 
+					callback({ "lat": res.lat(), "lng": res.lng(), "search":  address}); 
+				} else {
+					alert("Could not resolve "+address); 
+				}
+				
+			}); 
+
+					
+		}
+		
+		var marker = {
+			blank: function(data, color) {
+				return marker.base(data, 'markers/largeTD'+color+"Icons/blank.png"); 	            
+			}, 
+			
+			nextOf: function(data, color) {
+				if(!marker.colorCounters[color]) { 
+					marker.colorCounters[color]=1; 
+				} else { 
+					marker.colorCounters[color]++
+				}
+				var c  = marker.colorCounters[color];
+				return marker.base(data, 'markers/largeTD'+color+"Icons/marker"+c+".png"); 
+				
+			}, 
+			
+			base: function(data, icon) {
+				var size = new google.maps.Size(20, 34); 
+    	        // The origin for this image is 0,0.
+				var origin = new google.maps.Point(0,0);
+				// The anchor for this image is the base of the flagpole at 0,32.
+				var anchor = new google.maps.Point(10, 34);
+				
+				var image = new google.maps.MarkerImage(icon, size, origin, anchor); 
+				
+				var ll = new google.maps.LatLng(data.lat, data.lng);
+				var marker = new google.maps.Marker({position: ll, map: cfg.maps, title: data.search, icon: image});
+				return marker; 
+
+			}, 
+			
+			colorCounters : {}
+			
+			
+			
+		}; 
+    	
+    	
+    </script>
+  </body>
+</html>
